c2d5c5b4c8f7afaf6000d2690314df7ae2afad4e
Chandrakant Singh <aryalinux11@gmail.com>
2016-11-27 21:41:58 +0530

    Removed lightdm from kde installation

--- lfs/apps/kde.sh
+++ lfs/apps/kde.sh
@@ -10,7 +10,7 @@ alps updatescripts
 
 su - $USERNAME -c "env QT5DIR=/opt/qt5 QT5BINDIR=/opt/qt5 QT5PREFIX=/opt/qt5 alps install-no-prompt qt5"
 
-su - $USERNAME -c "env QT5DIR=/opt/qt5 QT5BINDIR=/opt/qt5 QT5PREFIX=/opt/qt5 PATH=$PATH:$QT5BINDIR alps install-no-prompt extra-cmake-modules phonon phonon-backend-gstreamer phonon-backend-vlc polkit-qt libdbusmenu-qt oxygen-fonts noto-fonts kframeworks5 ark5 kate5 kdenlive kmix5 khelpcenter konsole5 libkexiv2 okular5 libkdcraw gwenview5 plasma-all sddm"
+su - $USERNAME -c "env QT5DIR=/opt/qt5 QT5BINDIR=/opt/qt5 QT5PREFIX=/opt/qt5 PATH=$PATH:$QT5BINDIR alps install-no-prompt gtk2 gtk3 extra-cmake-modules phonon phonon-backend-gstreamer phonon-backend-vlc polkit-qt libdbusmenu-qt oxygen-fonts noto-fonts kframeworks5 ark5 kate5 kdenlive kmix5 khelpcenter konsole5 libkexiv2 okular5 libkdcraw gwenview5 plasma-all sddm"
 
 if ! grep "sddm=" /etc/alps/installed-list &> /dev/null
 then
