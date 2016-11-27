690a42a3be178ba916c8a4521b9ff9659bb51f36
Chandrakant Singh <aryalinux11@gmail.com>
2016-11-27 09:39:52 +0530

    Fixed extra-cmake-modules

--- blfs/extra-cmake-modules.sh
+++ blfs/extra-cmake-modules.sh
@@ -17,11 +17,11 @@ NAME="extra-cmake-modules"
 
 cd $SOURCE_DIR
 
-URL=http://download.kde.org/stable/frameworks/5.25/extra-cmake-modules-5.25.0.tar.xz
+URL=http://download.kde.org/stable/frameworks/5.27/extra-cmake-modules-5.27.0.tar.xz
 
 if [ ! -z $URL ]
 then
-wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/extra-cmake-modules/extra-cmake-modules-5.25.0.tar.xz || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/extra-cmake-modules/extra-cmake-modules-5.25.0.tar.xz || wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/extra-cmake-modules/extra-cmake-modules-5.25.0.tar.xz || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/extra-cmake-modules/extra-cmake-modules-5.25.0.tar.xz || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/extra-cmake-modules/extra-cmake-modules-5.25.0.tar.xz || wget -nc http://download.kde.org/stable/frameworks/5.25/extra-cmake-modules-5.25.0.tar.xz
+wget -nc http://download.kde.org/stable/frameworks/5.27/extra-cmake-modules-5.27.0.tar.xz
 
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 if [ -z $(echo $TARBALL | grep ".zip$") ]; then
