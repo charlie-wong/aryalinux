3ed7f1f01cf8f332ac175d3ece2b885c183f87a0
Chandrakant Singh <aryalinux11@gmail.com>
2016-11-30 08:39:10 +0530

    Reverted back versions of few kde-apps

--- blfs/kdenlive.sh
+++ blfs/kdenlive.sh
@@ -7,21 +7,23 @@ set +h
 . /var/lib/alps/functions
 
 SOURCE_ONLY=n
-DESCRIPTION="%DESCRIPTION%"
-SECTION="kde-apps"
-VERSION=16.08.3
+DESCRIPTION="br3ak The Kdenlive package is a KF5br3ak based video editor.br3ak"
+SECTION="kde"
+VERSION=16.08.0
 NAME="kdenlive"
 
-#REQ:plasma-all
+#REQ:mlt
+#REQ:v4l-utils
+#REQ:kframeworks5
 
 
 cd $SOURCE_DIR
 
-URL=http://download.kde.org/stable/applications/16.08.3/src/kdenlive-16.08.3.tar.xz
+URL=http://download.kde.org/stable/applications/16.08.0/src/kdenlive-16.08.0.tar.xz
 
 if [ ! -z $URL ]
 then
-wget -nc http://download.kde.org/stable/applications/16.08.3/src/kdenlive-16.08.3.tar.xz
+wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/kde/kdenlive-16.08.0.tar.xz || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/kde/kdenlive-16.08.0.tar.xz || wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/kde/kdenlive-16.08.0.tar.xz || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/kde/kdenlive-16.08.0.tar.xz || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/kde/kdenlive-16.08.0.tar.xz || wget -nc http://download.kde.org/stable/applications/16.08.0/src/kdenlive-16.08.0.tar.xz
 
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 if [ -z $(echo $TARBALL | grep ".zip$") ]; then
@@ -34,11 +36,26 @@ fi
 cd $DIRECTORY
 fi
 
-mkdir -pv build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DLIB_INSTALL_DIR=lib -DBUILD_TESTING=OFF -Wno-dev .. &&
-make -j$(nproc)
-sudo make install
+whoami > /tmp/currentuser
+
+mkdir build &&
+cd    build &&
+cmake -DCMAKE_INSTALL_PREFIX=/opt/kf5 \
+      -DCMAKE_BUILD_TYPE=Release         \
+      -DLIB_INSTALL_DIR=lib              \
+      -DBUILD_TESTING=OFF                \
+      -Wno-dev .. &&
+make "-j`nproc`" || make
+
+
+
+sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+make install
+
+ENDOFROOTSCRIPT
+sudo chmod 755 rootscript.sh
+sudo bash -e ./rootscript.sh
+sudo rm rootscript.sh
 
 
 
