70d531821b6d48993e3f1d2cd30435abab0a18ee
Chandrakant Singh <aryalinux11@gmail.com>
2016-12-07 21:00:30 +0530

    Fixed chrome

--- applications/chromium-bin.sh
+++ applications/chromium-bin.sh
@@ -13,12 +13,14 @@ export XORG_CONFIG="--prefix=$XORG_PREFIX --sysconfdir=/etc \
 #REQ:cups
 #REQ:GConf
 
-cd /opt
-sudo git clone https://github.com/scheib/chromium-latest-linux.git
+cd $SOURCE_DIR
+git clone https://github.com/scheib/chromium-latest-linux.git
 cd chromium-latest-linux
-sudo ./update.sh
+./update.sh
 
-sudo chmod a+rx /opt/chromium-latest-linux
+cd $SOURCE_DIR
+sudo cp -r chromium-latest-linux /opt/
+sudo chmod -R a+rx /opt/chromium-latest-linux
 
 sudo tee /etc/profile.d/chromium.sh <<"EOF"
 export PATH=$PATH:/opt/chromium-latest-linux/latest
