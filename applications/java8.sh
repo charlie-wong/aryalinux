f7b622fb702038ac15d1e14579920c0d2b68d08f
Chandrakant Singh <aryalinux11@gmail.com>
2016-12-07 12:43:32 +0530

    Fixed java

--- applications/java8.sh
+++ applications/java8.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
+
 set -e
 set +h
 
 . /etc/alps/alps.conf
 . /var/lib/alps/functions
 
+SOURCE_ONLY=n
+DESCRIPTION="br3ak Creating a JVM from source requires a set of circular dependencies.br3ak The first thing that's needed is a set of programs called a Javabr3ak Development Kit (JDK). This set of programs includes <span class=\"command\"><strong>java</strong>, <span class=\"command\"><strong>javac</strong>, <span class=\"command\"><strong>jar</strong>, and several others. It alsobr3ak includes several base <span class=\"emphasis\"><em>jar</em>br3ak files.br3ak"
+SECTION="Programming Languages"
+VERSION=1.8.0.112
+NAME="java8"
+
+#REQ:alsa-lib
+#REQ:cups
+#REQ:giflib
+#REQ:x7lib
+
+
 cd $SOURCE_DIR
 
-ARCH=$(uname -m)
-
-if [ $ARCH == "x86_64" ]
-then
-	URL=http://ftp.osuosl.org/pub/blfs/conglomeration/openjdk/OpenJDK-1.8.0.51-x86_64-bin.tar.xz
-else
-	URL=http://ftp.osuosl.org/pub/blfs/conglomeration/openjdk/OpenJDK-1.8.0.51-i686-bin.tar.xz
-fi
-
+URL=https://sourceforge.net/projects/aryalinux-bin/files/releases/2016.11/OpenJDK-1.8.0.112-x86_64-bin.tar.xz
 wget -nc $URL
-TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
-DIRECTORY=`tar -tf $TARBALL | sed -e 's@/.*@@' | uniq `
 
-tar -xf $TARBALL
+sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+tar xf OpenJDK-1.8.0.112-x86_64-bin.tar.xz -C /opt &&
+chown -R root:root /opt/OpenJDK-1.8.0.112-bin
+ln -sfn /opt/OpenJDK-1.8.0.112-bin /opt/jdk
 
-cd $DIRECTORY
-
-sudo install -v -dm755 /opt/OpenJDK-1.8.0.92-bin &&
-sudo mv -v * /opt/OpenJDK-1.8.0.92-bin         &&
-sudo chown -R root:root /opt/OpenJDK-1.8.0.92-bin
-
-sudo ln -sfnv OpenJDK-1.8.0.92-bin /opt/jdk
-
-sudo tee /etc/profile.d/openjdk.sh << "EOF"
+cat > /etc/profile.d/openjdk.sh << "EOF"
 # Begin /etc/profile.d/openjdk.sh
 
 # Set JAVA_HOME directory
@@ -41,12 +39,6 @@ pathappend $JAVA_HOME/bin
 
 # Add to MANPATH
 pathappend $JAVA_HOME/man MANPATH
-
-# Make sure C and C++ compilers can find Java headers
-pathappend $JAVA_HOME/include       C_INCLUDE_PATH
-pathappend $JAVA_HOME/include/linux C_INCLUDE_PATH
-pathappend $JAVA_HOME/include       CPLUS_INCLUDE_PATH
-pathappend $JAVA_HOME/include/linux CPLUS_INCLUDE_PATH
 
 # Auto Java CLASSPATH: Copy jar files to, or create symlinks in, the
 # /usr/share/java directory. Note that having gcj jars with OpenJDK 8
@@ -70,7 +62,7 @@ unset AUTO_CLASSPATH_DIR dir jar
 # End /etc/profile.d/openjdk.sh
 EOF
 
-sudo tee -a /etc/man_db.conf << "EOF" &&
+cat >> /etc/man_db.conf << "EOF" &&
 # Begin Java addition
 MANDATORY_MANPATH     /opt/jdk/man
 MANPATH_MAP           /opt/jdk/bin     /opt/jdk/man
@@ -78,10 +70,15 @@ MANDB_MAP             /opt/jdk/man     /var/cache/man/jdk
 # End Java addition
 EOF
 
-sudo mkdir -p /var/cache/man
-sudo mandb -c /opt/jdk/man
+mkdir -p /var/cache/man
+mandb -c /opt/jdk/man
 
-cd $SOURCE_DIR
-cleanup "$NAME" "$DIRECTORY"
+ENDOFROOTSCRIPT
+sudo chmod 755 rootscript.sh
+sudo bash -e ./rootscript.sh
+sudo rm rootscript.sh
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
 
 register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
