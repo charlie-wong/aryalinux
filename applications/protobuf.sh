22fcc5f98ce4df210a4e3a32b55793ef35f208fc
Chandrakant Singh <aryalinux11@gmail.com>
2016-12-15 08:36:56 +0530

    Fixed protobuf

--- applications/protobuf.sh
+++ applications/protobuf.sh
@@ -22,7 +22,7 @@ DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq`
 tar -xf $TARBALL
 cd $DIRECTORY
 
-./configure --prefix=/usr  &&
+./autogen.sh --prefix=/usr  &&
 make "-j`nproc`"
 sudo make install
 
