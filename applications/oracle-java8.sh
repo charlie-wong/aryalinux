1f9fa65d7832aedbd97b6d9146e087c45d21afb6
Chandrakant Singh <aryalinux11@gmail.com>
2016-12-16 16:42:48 +0530

    Added oracle-java8

---
+++ applications/oracle-java8.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+SOURCE_ONLY=n
+DESCRIPTION="Java is a platform independent Object oriented programming language"
+SECTION="Programming Languages"
+VERSION=1.8.0_112
+NAME="oracle-java8"
+
+#REQ:alsa-lib
+#REQ:cups
+#REQ:giflib
+#REQ:x7lib
+
+
+cd $SOURCE_DIR
+
+URL=http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.tar.gz
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $URL
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq)
+
+sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+tar xf $TARBALL -C /opt &&
+chown -R root:root /opt/$DIRECTORY
+ln -sfn /opt/$DIRECTORY /opt/jdk
+
+cat > /etc/profile.d/jdk.sh << "EOF"
+# Begin /etc/profile.d/jdk.sh
+
+# Set JAVA_HOME directory
+JAVA_HOME=/opt/jdk
+
+# Adjust PATH
+pathappend $JAVA_HOME/bin
+
+# Add to MANPATH
+pathappend $JAVA_HOME/man MANPATH
+
+# Auto Java CLASSPATH: Copy jar files to, or create symlinks in, the
+# /usr/share/java directory. Note that having gcj jars with OpenJDK 8
+# may lead to errors.
+
+AUTO_CLASSPATH_DIR=/usr/share/java
+
+pathprepend . CLASSPATH
+
+for dir in `find ${AUTO_CLASSPATH_DIR} -type d 2>/dev/null`; do
+    pathappend $dir CLASSPATH
+done
+
+for jar in `find ${AUTO_CLASSPATH_DIR} -name "*.jar" 2>/dev/null`; do
+    pathappend $jar CLASSPATH
+done
+
+export JAVA_HOME
+unset AUTO_CLASSPATH_DIR dir jar
+
+# End /etc/profile.d/jdk.sh
+EOF
+
+cat >> /etc/man_db.conf << "EOF" &&
+# Begin Java addition
+MANDATORY_MANPATH     /opt/jdk/man
+MANPATH_MAP           /opt/jdk/bin     /opt/jdk/man
+MANDB_MAP             /opt/jdk/man     /var/cache/man/jdk
+# End Java addition
+EOF
+
+mkdir -p /var/cache/man
+mandb -c /opt/jdk/man
+
+ENDOFROOTSCRIPT
+sudo chmod 755 rootscript.sh
+sudo bash -e ./rootscript.sh
+sudo rm rootscript.sh
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
