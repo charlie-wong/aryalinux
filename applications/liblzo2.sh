b163dc2664c7c02eca9ad193671a7d34388f63c3
Chandrakant Singh <aryalinux11@gmail.com>
2016-12-24 04:08:51 +0530

    Fixed liblz02

--- applications/liblzo2.sh
+++ applications/liblzo2.sh
@@ -6,8 +6,8 @@ set +h
 . /etc/alps/alps.conf
 . /var/lib/alps/functions
 
-NAME="lzo.orig"
-VERSION="2_2.08"
+NAME="liblzo2"
+VERSION="2.08"
 
 URL=http://archive.ubuntu.com/ubuntu/pool/main/l/lzo2/lzo2_2.08.orig.tar.gz
 
