eb3024bad95a2aa683b1be4555ca583deb3fcbef
Chandrakant Singh <aryalinux11@gmail.com>
2017-01-16 12:27:25 +0530

    Added python-modules#pandas

---
+++ applications/python-modules#pandas.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+SOURCE_ONLY=n
+DESCRIPTION="%DESCRIPTION%"
+SECTION="general"
+VERSION=3.6.4
+NAME="python-modules#pandas"
+
+cd $SOURCE_DIR
+
+URL=https://pypi.python.org/packages/08/9d/31ec596099f14528fc6ad39428248ac5360f0bb5205a3ee79a5d1cf260fb/pandas-0.19.2.tar.gz
+
+if [ ! -z $URL ]
+then
+wget -nc $URL
+
+TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$"`
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+cd $DIRECTORY
+fi
+
+
+sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+python setup.py build &&
+python setup.py install --optimize=1
+ENDOFROOTSCRIPT
+sudo chmod 755 rootscript.sh
+sudo bash -e ./rootscript.sh
+sudo rm rootscript.sh
+
+
+
+sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+python3 setup.py clean &&
+python3 setup.py build &&
+python3 setup.py install --optimize=1
+ENDOFROOTSCRIPT
+sudo chmod 755 rootscript.sh
+sudo bash -e ./rootscript.sh
+sudo rm rootscript.sh
+
+
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
