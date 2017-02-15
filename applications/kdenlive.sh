df2fb211fe8663923649c63b7f18170c0afa422a
Chandrakant Singh <cks3383@gmail.com>
2017-02-15 20:05:34 +0530

    Upped kdenlive version

--- applications/kdenlive.sh
+++ applications/kdenlive.sh
@@ -9,7 +9,7 @@ set +h
 SOURCE_ONLY=n
 DESCRIPTION="br3ak The Kdenlive package is a KF5br3ak based video editor.br3ak"
 SECTION="kde"
-VERSION=16.08.3
+VERSION=16.12.2
 NAME="kdenlive"
 
 #REQ:mlt
@@ -19,11 +19,11 @@ NAME="kdenlive"
 
 cd $SOURCE_DIR
 
-URL=http://download.kde.org/stable/applications/16.08.3/src/kdenlive-16.08.3.tar.xz
+URL=http://download.kde.org/stable/applications/16.08.3/src/kdenlive-16.12.2.tar.xz
 
 if [ ! -z $URL ]
 then
-wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/kde/kdenlive-16.08.3.tar.xz || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/kde/kdenlive-16.08.3.tar.xz || wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/kde/kdenlive-16.08.3.tar.xz || wget -nc http://download.kde.org/stable/applications/16.08.3/src/kdenlive-16.08.3.tar.xz || wget -nc http://mirrors-ru.go-parts.com/blfs/conglomeration/kde/kdenlive-16.08.3.tar.xz || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/kde/kdenlive-16.08.3.tar.xz || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/kde/kdenlive-16.08.3.tar.xz
+wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/kde/kdenlive-16.12.2.tar.xz
 
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 if [ -z $(echo $TARBALL | grep ".zip$") ]; then
