1b952c1cdec0c29393befc768a76d19beedf66d3
Chandrakant Singh <cks3383@gmail.com>
2017-02-19 00:22:35 +0530

    Upped kde-apps versions

--- applications/kate.sh
+++ applications/kate.sh
@@ -9,7 +9,7 @@ set +h
 SOURCE_ONLY=n
 DESCRIPTION="%DESCRIPTION%"
 SECTION="kde-apps"
-VERSION=16.08.0
+VERSION=16.12.2
 NAME="kate"
 
 #REQ:plasma-all
@@ -17,11 +17,11 @@ NAME="kate"
 
 cd $SOURCE_DIR
 
-URL=http://download.kde.org/stable/applications/16.08.0/src/kate-16.08.0.tar.xz
+URL=http://download.kde.org/stable/applications/16.12.2/src/kate-16.12.2.tar.xz
 
 if [ ! -z $URL ]
 then
-wget -nc http://download.kde.org/stable/applications/16.08.0/src/kate-16.08.0.tar.xz
+wget -nc http://download.kde.org/stable/applications/16.12.2/src/kate-16.12.2.tar.xz
 
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 if [ -z $(echo $TARBALL | grep ".zip$") ]; then
