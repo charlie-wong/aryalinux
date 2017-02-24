edc178308c253d104977100b5c68dbef8e04057a
Chandrakant Singh <cks3383@gmail.com>
2017-02-24 18:45:03 +0530

    Added perl-modules#xml-sax-base.sh

---
+++ applications/perl-modules#xml-sax-base.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+
+NAME="perl-modules#xml-sax-base"
+VERSION="1.08"
+DESCRIPTION="XML::SAX::Base is intended for use as a base class for SAX filter modules and XML parsers generating SAX events"
+SECTION="perl-modules"
+
+URL=http://search.cpan.org/CPAN/authors/id/G/GR/GRANTM/XML-SAX-Base-1.08.tar.gz
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+
+cd $SOURCE_DIR
+wget -nc $URL
+DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq)
+
+tar xf $TARBALL
+cd $DIRECTORY
+
+perl Makefile.PL &&
+make
+sudo make install
+
+cd $SOURCE_DIR
+sudo rm -r $DIRECTORY
+
+register_installed "$NAME=>`date`" "$VERSION" "$INSTALLED_LIST"
