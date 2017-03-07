d0b54cb83503ff7c6199010115342e4db7cd5eb9
Chandrakant Singh <aryalinux11@gmail.com>
2017-03-07 20:28:38 +0530

    Updated linux version for virtualbox

--- applications/virtualbox.sh
+++ applications/virtualbox.sh
@@ -16,17 +16,17 @@ then
 else
 	wget -nc http://download.virtualbox.org/virtualbox/5.1.0/VirtualBox-5.1.0-108711-Linux_x86.run
 fi
-wget -nc https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.7.tar.xz
-tar xf linux-4.7.tar.xz
+wget -nc https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.9.tar.xz
+tar xf linux-4.9.tar.xz
 if [ `uname -m` == "x86_64" ]
 then
 	wget -nc https://raw.githubusercontent.com/FluidIdeas/aryalinux/2016.08/lfs/en/config-64
-	cp config-64 linux-4.7/.config
+	cp config-64 linux-4.9/.config
 else
 	wget -nc https://raw.githubusercontent.com/FluidIdeas/aryalinux/2016.08/lfs/en/config-32
-	cp config-32 linux-4.7/.config
+	cp config-32 linux-4.9/.config
 fi
-pushd linux-4.7
+pushd linux-4.9
 make oldconfig
 make prepare
 make scripts
@@ -37,7 +37,7 @@ then
 else
 	chmod a+x VirtualBox-5.1.0-108711-Linux_x86.run
 fi
-sudo KERN_DIR="`pwd`/linux-4.7" ./VirtualBox-5.1.0-108711-Linux_amd64.run
+sudo KERN_DIR="`pwd`/linux-4.9" ./VirtualBox-5.1.0-108711-Linux_amd64.run
 
 sudo ln -svf /opt/VirtualBox/virtualbox.desktop /usr/share/applications/
 sudo update-desktop-database
