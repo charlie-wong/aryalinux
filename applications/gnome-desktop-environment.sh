530010d62fda9c777aa7f4f2f17fab6ea1be12df
Chandrakant Singh <cks3383@gmail.com>
2017-04-23 17:08:54 +0530

    Removed lightdm as display manager for gnome

--- applications/gnome-desktop-environment.sh
+++ applications/gnome-desktop-environment.sh
@@ -63,7 +63,7 @@ DESCRIPTION="GNOME is a desktop environment that is composed entirely of free an
 #REQ:gnome-shell-extensions
 #REQ:gnome-session
 #REQ:plymouth
-#REQ:lightdm-gtk-greeter
+#OPT:lightdm-gtk-greeter
 #REQ:gnome-user-docs
 #OPT:yelp
 #REQ:baobab
