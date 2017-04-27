f7dac7463e7143ea173e261a283fc1f19781914c
Chandrakant Singh <cks3383@gmail.com>
2017-04-27 23:26:03 +0530

    Upped gnome-shell version

--- applications/gnome-shell.sh
+++ applications/gnome-shell.sh
@@ -9,7 +9,7 @@ set +h
 SOURCE_ONLY=n
 DESCRIPTION="br3ak The GNOME Shell is the core userbr3ak interface of the GNOME Desktopbr3ak environment.br3ak"
 SECTION="gnome"
-VERSION=3.22.3
+VERSION=3.23.1
 NAME="gnome-shell"
 
 #REQ:caribou
@@ -35,7 +35,7 @@ NAME="gnome-shell"
 
 cd $SOURCE_DIR
 
-URL=http://ftp.gnome.org/pub/gnome/sources/gnome-shell/3.22.3/gnome-shell-3.22.3.tar.xz
+URL=http://ftp.gnome.org/pub/gnome/sources/gnome-shell/3.23/gnome-shell-3.23.1.tar.xz
 
 if [ ! -z $URL ]
 then
