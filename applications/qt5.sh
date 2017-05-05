7e9e1fb19e83f7b69d654792af5ca7c4047c90d7
Chandrakant Singh <aryalinux11@gmail.com>
2017-05-05 20:38:20 +0530

    Fixed qt5 package building

--- applications/qt5.sh
+++ applications/qt5.sh
@@ -99,7 +99,84 @@ make "-j`nproc`" || make
 sudo mkdir -pv /var/cache/alps/binaries
 sudo chmod a+rw /var/cache/alps/binaries
 INSTALL_DIR=/var/cache/alps/binaries/$NAME-$VERSION-$(uname -m)
-make DESTDIR=${INSTALL_DIR} install
+make INSTALL_ROOT=${INSTALL_DIR} install
+find ${INSTALL_DIR}/opt/qt5/lib/pkgconfig -name "*.pc" -exec perl -pi -e "s, -L$PWD/?\S+,,g" {} \;
+find ${INSTALL_DIR}/opt/qt5/ -name qt_lib_bootstrap_private.pri \
+   -exec sed -i -e "s:$PWD/qtbase://opt/qt5/lib/:g" {} \; &&
+find ${INSTALL_DIR}/opt/qt5/ -name \*.prl \
+   -exec sed -i -e '/^QMAKE_PRL_BUILD_DIR/d' {} \;
+
+install -v -dm755 ${INSTALL_DIR}/usr/share/pixmaps/                  &&
+install -v -Dm644 qttools/src/assistant/assistant/images/assistant-128.png \
+                  ${INSTALL_DIR}/usr/share/pixmaps/assistant-qt5.png &&
+install -v -Dm644 qttools/src/designer/src/designer/images/designer.png \
+                  ${INSTALL_DIR}/usr/share/pixmaps/designer-qt5.png  &&
+install -v -Dm644 qttools/src/linguist/linguist/images/icons/linguist-128-32.png \
+                  ${INSTALL_DIR}/usr/share/pixmaps/linguist-qt5.png  &&
+install -v -Dm644 qttools/src/qdbus/qdbusviewer/images/qdbusviewer-128.png \
+                  ${INSTALL_DIR}/usr/share/pixmaps/qdbusviewer-qt5.png &&
+install -dm755 ${INSTALL_DIR}/usr/share/applications &&
+cat > ${INSTALL_DIR}/usr/share/applications/assistant-qt5.desktop << EOF
+[Desktop Entry]
+Name=Qt5 Assistant
+Comment=Shows Qt5 documentation and examples
+Exec=/opt/qt5/bin/assistant
+Icon=assistant-qt5.png
+Terminal=false
+Encoding=UTF-8
+Type=Application
+Categories=Qt;Development;Documentation;
+EOF
+cat > ${INSTALL_DIR}/usr/share/applications/designer-qt5.desktop << EOF
+[Desktop Entry]
+Name=Qt5 Designer
+GenericName=Interface Designer
+Comment=Design GUIs for Qt5 applications
+Exec=/opt/qt5/bin/designer
+Icon=designer-qt5.png
+MimeType=application/x-designer;
+Terminal=false
+Encoding=UTF-8
+Type=Application
+Categories=Qt;Development;
+EOF
+cat > ${INSTALL_DIR}/usr/share/applications/linguist-qt5.desktop << EOF
+[Desktop Entry]
+Name=Qt5 Linguist
+Comment=Add translations to Qt5 applications
+Exec=/opt/qt5/bin/linguist
+Icon=linguist-qt5.png
+MimeType=text/vnd.trolltech.linguist;application/x-linguist;
+Terminal=false
+Encoding=UTF-8
+Type=Application
+Categories=Qt;Development;
+EOF
+cat > ${INSTALL_DIR}/usr/share/applications/qdbusviewer-qt5.desktop << EOF
+[Desktop Entry]
+Name=Qt5 QDbusViewer
+GenericName=D-Bus Debugger
+Comment=Debug D-Bus applications
+Exec=/opt/qt5/bin/qdbusviewer
+Icon=qdbusviewer-qt5.png
+Terminal=false
+Encoding=UTF-8
+Type=Application
+Categories=Qt;Development;Debugger;
+EOF
+
+for file in moc uic rcc qmake lconvert lrelease lupdate; do
+  ln -sfrvn ${INSTALL_DIR}/opt/qt5/bin/$file ${INSTALL_DIR}/usr/bin/$file-qt5
+done
+
+cat > ${INSTALL_DIR}/etc/profile.d/qt5.sh << "EOF"
+# Begin /etc/profile.d/qt5.sh
+QT5DIR=/opt/qt5
+pathappend /opt/qt5/bin PATH
+pathappend /opt/qt5/lib/pkgconfig PKG_CONFIG_PATH
+export QT5DIR
+# End /etc/profile.d/qt5.sh
+EOF
 
 pushd ${INSTALL_DIR}
 tar -cJvf ${INSTALL_DIR}/../$NAME-$VERSION-$(uname -m).tar.xz *
