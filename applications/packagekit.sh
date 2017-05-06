66a1db2588d2313774a3e1c92bbf34d51cc6fdd2
Chandrakant Singh <aryalinux11@gmail.com>
2017-05-06 09:45:11 +0530

    Fixed packagekit

--- applications/packagekit.sh
+++ applications/packagekit.sh
@@ -6,13 +6,13 @@ set +h
 . /var/lib/alps/functions
 
 NAME="PackageKit"
-VERSION="1.1.1"
+VERSION="1.1.5"
 
 #REQ:bash-completion
 
 cd $SOURCE_DIR
 
-URL=https://www.freedesktop.org/software/PackageKit/releases/PackageKit-1.1.1.tar.xz
+URL=https://www.freedesktop.org/software/PackageKit/releases/PackageKit-1.1.5.tar.xz
 wget -nc $URL
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 DIRECTORY=`tar -tf $TARBALL | sed -e 's@/.*@@' | uniq `
@@ -23,7 +23,9 @@ cd $DIRECTORY
 
 ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-static &&
 make
-sudo make install
+
+makepkg "$NAME" "$VERSION" "1"
+sudo tar xf $BINARY_DIR/$NAME-$VERSION-$(uname -m).tar.xz -C /
 
 cd $SOURCE_DIR
 cleanup "$NAME" "$DIRECTORY"
