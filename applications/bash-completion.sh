5f0a9c37459512d78510d984f9dc558e185271f4
Chandrakant Singh <aryalinux11@gmail.com>
2017-05-06 13:30:02 +0530

    Fixed scripts

--- applications/bash-completion.sh
+++ applications/bash-completion.sh
@@ -5,7 +5,7 @@ set +h
 . /etc/alps/alps.conf
 . /var/lib/alps/functions
 
-NAME="bash-completion_.orig"
+NAME="bash-completion"
 VERSION="2.1"
 
 cd $SOURCE_DIR
@@ -21,7 +21,9 @@ cd $DIRECTORY
 
 ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-static &&
 make
-sudo make install
+
+makepkg "$NAME" "$VERSION" "1"
+sudo tar xf $BINARY_DIR/$NAME-$VERSION-$(uname -m).tar.xz -C /
 
 cd $SOURCE_DIR
 cleanup "$NAME" "$DIRECTORY"
