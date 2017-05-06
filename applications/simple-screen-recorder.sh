414f7c2a7191d0cb560bdae5ce9b9d3fe2a8f514
Chandrakant Singh <aryalinux11@gmail.com>
2017-05-06 17:37:00 +0530

    Fixed simple-screen-recorder and added package support

--- applications/simple-screen-recorder.sh
+++ applications/simple-screen-recorder.sh
@@ -10,9 +10,13 @@ set +h
 #REQ:ffmpeg
 #REQ:pulseaudio
 
+NAME="simple-screen-recorder"
+VERSION="0.3.8"
+DESCRIPTION="SimpleScreenRecorder is a screen recorder for Linux. Despite the name, this program is actually quite complex. It's 'simple' in the sense that it's easier to use than ffmpeg/avconv or VLC :)"
+
 cd $SOURCE_DIR
 
-URL=https://github.com/MaartenBaert/ssr/archive/master.tar.gz
+URL=https://sourceforge.net/projects/aryalinux-bin/files/releases/2017.05/simple-screen-recorder-0.3.8.tar.gz
 wget -nc $URL
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 DIRECTORY=`tar -tf $TARBALL | sed -e 's@/.*@@' | uniq `
@@ -30,7 +34,9 @@ export PKG_CONFIG_PATH="/usr/lib/pkgconfig:/opt/qt5/lib/pkgconfig"
 
 CXXFLAGS="-fPIC" ./configure --prefix=/usr --with-qt5 --with-jack --with-pulseaudio &&
 make "-j`nproc`"
-sudo make install
+
+makepkg "$NAME" "$VERSION" "1"
+sudo tar xf $BINARY_DIR/$NAME-$VERSION-$(uname -m).tar.xz -C /
 
 cd $SOURCE_DIR
 cleanup "$NAME" "$DIRECTORY"
