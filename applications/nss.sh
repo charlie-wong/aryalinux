d190dd936d7c1a1c82c7b34800f0335e377a7642
Chandrakant Singh <cks3383@gmail.com>
2017-05-15 19:40:40 +0530

    Fixed nss

--- applications/nss.sh
+++ applications/nss.sh
@@ -9,7 +9,7 @@ set +h
 SOURCE_ONLY=n
 DESCRIPTION="br3ak The Network Security Services (NSS) package is a set of libraries designed tobr3ak support cross-platform development of security-enabled client andbr3ak server applications. Applications built with NSS can support SSL v2br3ak and v3, TLS, PKCS #5, PKCS #7, PKCS #11, PKCS #12, S/MIME, X.509 v3br3ak certificates, and other security standards. This is useful forbr3ak implementing SSL and S/MIME or other Internet security standardsbr3ak into an application.br3ak"
 SECTION="postlfs"
-VERSION=3.30.1
+VERSION=3.30.2
 NAME="nss"
 
 #REQ:nspr
@@ -19,12 +19,12 @@ NAME="nss"
 
 cd $SOURCE_DIR
 
-URL=https://ftp.mozilla.org/pub/mozilla.org/security/nss/releases/NSS_3_30_1_RTM/src/nss-3.30.1.tar.gz
+URL=https://ftp.mozilla.org/pub/mozilla.org/security/nss/releases/NSS_3_30_2_RTM/src/nss-3.30.2.tar.gz
 
 if [ ! -z $URL ]
 then
-wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/nss/nss-3.30.1.tar.gz || wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/nss/nss-3.30.1.tar.gz || wget -nc https://ftp.mozilla.org/pub/mozilla.org/security/nss/releases/NSS_3_30_1_RTM/src/nss-3.30.1.tar.gz || wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/nss/nss-3.30.1.tar.gz || wget -nc http://mirrors-ru.go-parts.com/blfs/conglomeration/nss/nss-3.30.1.tar.gz || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/nss/nss-3.30.1.tar.gz || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/nss/nss-3.30.1.tar.gz
-wget -nc http://www.linuxfromscratch.org/patches/downloads/nss/nss-3.30.1-standalone-1.patch || wget -nc http://www.linuxfromscratch.org/patches/blfs/svn/nss-3.30.1-standalone-1.patch
+wget -nc https://ftp.mozilla.org/pub/mozilla.org/security/nss/releases/NSS_3_30_2_RTM/src/nss-3.30.2.tar.gz
+wget -nc http://www.linuxfromscratch.org/patches/blfs/svn/nss-3.30.2-standalone-1.patch
 
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 if [ -z $(echo $TARBALL | grep ".zip$") ]; then
@@ -39,47 +39,36 @@ fi
 
 whoami > /tmp/currentuser
 
-patch -Np1 -i ../nss-3.30.1-standalone-1.patch &&
+patch -Np1 -i ../nss-3.30.2-standalone-1.patch &&
 cd nss &&
-CFLAGS="-Wno-error=int-in-bool-context" make -j1 BUILD_OPT=1                  \
+make -j1 BUILD_OPT=1                  \
   NSPR_INCLUDE_DIR=/usr/include/nspr  \
   USE_SYSTEM_ZLIB=1                   \
   ZLIB_LIBS=-lz                       \
+  NSS_ENABLE_WERROR=0                 \
   $([ $(uname -m) = x86_64 ] && echo USE_64=1) \
   $([ -f /usr/include/sqlite3.h ] && echo NSS_USE_SYSTEM_SQLITE=1)
 
-
-
-sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
 cd ../dist                                                          &&
-install -v -m755 Linux*/lib/*.so              /usr/lib              &&
-install -v -m644 Linux*/lib/{*.chk,libcrmf.a} /usr/lib              &&
-install -v -m755 -d                           /usr/include/nss      &&
-cp -v -RL {public,private}/nss/*              /usr/include/nss      &&
-chmod -v 644                                  /usr/include/nss/*    &&
-install -v -m755 Linux*/bin/{certutil,nss-config,pk12util} /usr/bin &&
-install -v -m644 Linux*/lib/pkgconfig/nss.pc  /usr/lib/pkgconfig
 
-ENDOFROOTSCRIPT
-sudo chmod 755 rootscript.sh
-sudo bash -e ./rootscript.sh
-sudo rm rootscript.sh
+sudo install -v -m755 Linux*/lib/*.so              /usr/lib              &&
+sudo install -v -m644 Linux*/lib/{*.chk,libcrmf.a} /usr/lib              &&
 
+sudo install -v -m755 -d                           /usr/include/nss      &&
+sudo cp -v -RL {public,private}/nss/*              /usr/include/nss      &&
+sudo chmod -v 644                                  /usr/include/nss/*    &&
 
+sudo install -v -m755 Linux*/bin/{certutil,nss-config,pk12util} /usr/bin &&
 
-sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+sudo install -v -m644 Linux*/lib/pkgconfig/nss.pc  /usr/lib/pkgconfig
+
+cd ../dist                                                          &&
+
 if [ -e /usr/lib/libp11-kit.so ]; then
   readlink /usr/lib/libnssckbi.so ||
-  rm -v /usr/lib/libnssckbi.so    &&
-  ln -sfv libp11-kit.so /usr/lib/libnssckbi.so
+  sudo m -v /usr/lib/libnssckbi.so    &&
+  sudo ln -sfv libp11-kit.so /usr/lib/libnssckbi.so
 fi
-
-ENDOFROOTSCRIPT
-sudo chmod 755 rootscript.sh
-sudo bash -e ./rootscript.sh
-sudo rm rootscript.sh
-
-
 
 
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
