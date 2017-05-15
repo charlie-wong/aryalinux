bd52cf2e7236b7607702bcca90f359efe2337002
Chandrakant Singh <cks3383@gmail.com>
2017-05-16 04:51:52 +0530

    Bumped versions of mate components

--- applications/mate-menus.sh
+++ applications/mate-menus.sh
@@ -9,11 +9,11 @@ set +h
 SOURCE_ONLY=n
 NAME="mate-menus"
 DESCRIPTION="Implementation of the freedesktop menu specification for MATE"
-VERSION="1.16.0"
+VERSION="1.18.0"
 
 cd $SOURCE_DIR
 
-URL="http://pub.mate-desktop.org/releases/1.16/mate-menus-1.16.0.tar.xz"
+URL="http://pub.mate-desktop.org/releases/1.18/mate-menus-1.18.0.tar.xz"
 wget -nc $URL
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 DIRECTORY=`tar -tf $TARBALL | cut -d/ -f1 | uniq`
@@ -24,7 +24,8 @@ cd $DIRECTORY
 ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-static --with-gtk=3.0 &&
 make "-j`nproc`"
 
-sudo make install
+makepkg "$NAME" "$VERSION" "1"
+sudo tar xf $BINARY_DIR/$NAME-$VERSION-$(uname -m).tar.xz -C /
 
 cd $SOURCE_DIR
 
