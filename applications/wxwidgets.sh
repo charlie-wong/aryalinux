51286eddbc867e3cb0b8572195c89f9aea688b38
Chandrakant Singh <aryalinux11@gmail.com>
2017-05-16 23:37:51 +0530

    Fixed wxwidgets

--- applications/wxwidgets.sh
+++ applications/wxwidgets.sh
@@ -9,11 +9,15 @@ DESCRIPTION="A cross platform UI library for which bindings are available in C++
 NAME="wxwidgets"
 VERSION="3.0.2"
 
+#REQ:gtk2
+#REQ:gtk3
+#REQ:glu
+#REQ:sdl
+
 cd $SOURCE_DIR
 
 URL=https://github.com/wxWidgets/wxWidgets/releases/download/v3.0.2/wxWidgets-3.0.2.tar.bz2
 wget -nc $URL
-wget -nc https://sourceforge.net/projects/aryalinux-bin/files/artifacts/wxwidgets-3.0.2-scintilla.patch
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 DIRECTORY=`tar -tf $TARBALL | sed -e 's@/.*@@' | uniq `
 
@@ -21,12 +25,9 @@ tar -xf $TARBALL
 
 cd $DIRECTORY
 
-./autogen.sh --prefix=/usr
-./configure --prefix=/usr &&
-patch -Np1 -i ../wxwidgets-3.0.2-scintilla.patch &&
+./configure --with-opengl --prefix=/usr --with-sdl &&
 make "-j`nproc`"
-makepkg "$NAME" "$VERSION" "1"
-sudo tar xf $BINARY_DIR/$NAME-$VERSION-$(uname -m).tar.xz -C /
+sudo make install
 
 cd $SOURCE_DIR
 cleanup "$NAME" "$DIRECTORY"
