a44b85b35a540c551a72181034812f9547306775
Chandrakant Singh <aryalinux11@gmail.com>
2017-05-25 15:23:44 +0530

    Fixed thunar-archive-plugin

--- applications/thunar-archive-plugin.sh
+++ applications/thunar-archive-plugin.sh
@@ -11,7 +11,7 @@ set +h
 NAME=thunar-archive-plugin
 VERSION=0.3.1
 DESCRIPTION="The Thunar Archive Plugin allows you to create and extract archive files using the file context menus in the Thunar file manager."
-URL=https://git.xfce.org/thunar-plugins/thunar-archive-plugin/snapshot/thunar-archive-plugin-0.3.1.tar.bz2
+URL=http://archive.ubuntu.com/ubuntu/pool/universe/t/thunar-archive-plugin/thunar-archive-plugin_0.3.1.orig.tar.bz2
 
 cd $SOURCE_DIR
 wget $URL
