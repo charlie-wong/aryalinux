eb459ccb65f297efaffb4f5aa40ac080566b60da
Chandrakant Singh <aryalinux11@gmail.com>
2017-05-25 14:59:20 +0530

    Fixed thunar-archive-plugin

--- applications/cacerts.sh
+++ applications/cacerts.sh
@@ -7,10 +7,9 @@ set +h
 . /var/lib/alps/functions
 
 SOURCE_ONLY=n
-DESCRIPTION="Public Key Infrastructure (PKI) is a method to validate the authenticity of an otherwise unknown entity across untrusted networks. PKI works by establishing a chain of trust, rather than trusting each individual host or entity explicitly. In order for a certificate presented by a remote entity to be trusted, that certificate must present a complete chain of certificates that can be validated using the root certificate of a Certificate Authority (CA) that is trusted by the local machine"
+DESCRIPTION="br3ak Download (HTTP): <a class=\"ulink\" href=\"http://anduin.linuxfromscratch.org/BLFS/other/make-ca.sh-20170119\">br3ak http://anduin.linuxfromscratch.org/BLFS/other/make-ca.sh-20170119</a>br3ak"
 SECTION="postlfs"
 NAME="cacerts"
-VERSION="latest"
 
 #REQ:openssl
 #OPT:java
@@ -20,11 +19,44 @@ VERSION="latest"
 
 cd $SOURCE_DIR
 
-wget -nc http://anduin.linuxfromscratch.org/BLFS/other/make-ca.sh-20161126
-wget -nc http://anduin.linuxfromscratch.org/BLFS/other/certdata.txt
+URL=
 
-sudo install -vm755 make-ca.sh-20161126 /usr/sbin/make-ca.sh
-sudo /usr/sbin/make-ca.sh
+if [ ! -z $URL ]
+then
+
+TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$"`
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+cd $DIRECTORY
+fi
+
+whoami > /tmp/currentuser
+
+
+sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+install -vm755 make-ca.sh-20170119 /usr/sbin/make-ca.sh
+
+ENDOFROOTSCRIPT
+sudo chmod 755 rootscript.sh
+sudo bash -e ./rootscript.sh
+sudo rm rootscript.sh
+
+
+
+sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+/usr/sbin/make-ca.sh
+
+ENDOFROOTSCRIPT
+sudo chmod 755 rootscript.sh
+sudo bash -e ./rootscript.sh
+sudo rm rootscript.sh
+
+
 
 
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
--- applications/ninja.sh
+++ applications/ninja.sh
@@ -6,27 +6,84 @@ set +h
 . /etc/alps/alps.conf
 . /var/lib/alps/functions
 
-NAME=ninja
-VERSION=1.7.2
-DESCRIPTION="Ninja is a small build system with a focus on speed."
 SOURCE_ONLY=n
+DESCRIPTION="br3ak Ninja is a small build system withbr3ak a focus on speed.br3ak"
+SECTION="general"
+VERSION=1.7.2
+NAME="ninja"
 
 #REQ:python2
+#OPT:emacs
+#OPT:asciidoc
+#OPT:doxygen
 
-URL="https://github.com/ninja-build/ninja/archive/v1.7.2.tar.gz"
-TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
 
 cd $SOURCE_DIR
 
-wget -nc $URL
+URL=https://github.com/ninja-build/ninja/archive/v1.7.2.tar.gz
 
-DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq)
+if [ ! -z $URL ]
+then
+wget -nc https://github.com/ninja-build/ninja/archive/v1.7.2.tar.gz
 
-tar xf $TARBALL
+TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$"`
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
 cd $DIRECTORY
+fi
+
+whoami > /tmp/currentuser
+
+wget https://github.com/ninja-build/ninja/archive/v1.7.2.tar.gz \
+     -O ninja-1.7.2.tar.gz
+
 
 ./configure.py --bootstrap
-sudo cp ninja /usr/bin
+
+
+emacs -Q --batch -f batch-byte-compile misc/ninja-mode.el
+
+
+./configure.py &&
+./ninja ninja_test &&
+./ninja_test --gtest_filter=-SubprocessTest.SetWithLots
+
+
+
+sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+install -vm755 ninja /usr/bin/ &&
+install -vDm644 misc/ninja.vim \
+                /usr/share/vim/vim74/syntax/ninja.vim &&
+install -vDm644 misc/bash-completion \
+                /usr/share/bash-completion/completions/ninja &&
+install -vDm644 misc/zsh-completion \
+                /usr/share/zsh/site-functions/_ninja
+
+ENDOFROOTSCRIPT
+sudo chmod 755 rootscript.sh
+sudo bash -e ./rootscript.sh
+sudo rm rootscript.sh
+
+
+
+sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+install -vDm644 misc/ninja-mode.el \
+                /usr/share/emacs/site-lisp/ninja-mode.el
+install -vDm644 misc/ninja-mode.elc \
+                /usr/share/emacs/site-lisp/ninja-mode.elc
+
+ENDOFROOTSCRIPT
+sudo chmod 755 rootscript.sh
+sudo bash -e ./rootscript.sh
+sudo rm rootscript.sh
+
+
+
 
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
 
